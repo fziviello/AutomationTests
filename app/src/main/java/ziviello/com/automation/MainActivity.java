1355b0041332cf110c6f1d21d1ab646c854d61b5
Fabio Ziviello <f.ziviello@email.it>
2018-06-21 16:06:21 +0200

    Add Test HTTP

--- app/src/main/java/ziviello/com/automation/MainActivity.java
+++ app/src/main/java/ziviello/com/automation/MainActivity.java
@@ -5,7 +5,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +23,20 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn = (Button) findViewById(R.id.button1);
         Button btn2 = (Button) findViewById(R.id.button2);
+        Button btn3 = (Button) findViewById(R.id.button3);
+
+        final ImageView img1 = (ImageView) findViewById(R.id.img1);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(MainActivity.this, "Click BTN1", Toast.LENGTH_SHORT).show();
-                Log.e("APP","Click sul BTN_1");
+                Toast.makeText(MainActivity.this, "Foto Caricata", Toast.LENGTH_SHORT).show();
+                Log.i("APP", "Click sul BTN_1");
+
+                img1.setBackgroundResource(R.drawable.ic_launcher_background);
+
+
             }
         });
 
@@ -30,10 +44,40 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(MainActivity.this, "Click BTN2", Toast.LENGTH_SHORT).show();
-                Log.e("APP","Click sul BTN_2");
+                Toast.makeText(MainActivity.this, "Foto Eliminata", Toast.LENGTH_SHORT).show();
+                Log.i("APP","Click sul BTN_2");
+                img1.setBackgroundResource(0);
             }
         });
 
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.i("APP","Click sul BTN_3");
+
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url ="http://rw";
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Toast.makeText(MainActivity.this, response.substring(0,500), Toast.LENGTH_SHORT).show();
+                        }
+                    }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(MainActivity.this, "Errore Chiamata HTTP", Toast.LENGTH_SHORT).show();
+                        Log.e("ErroreHttp:", String.valueOf(error));
+                    }
+                });
+
+                queue.add(stringRequest);
+            }
+
+            });
+
     }
+
+
 }
--- app/src/test/java/ziviello/com/automation/MainTests.java
+++ app/src/test/java/ziviello/com/automation/MainTests.java
@@ -5,12 +5,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.logging.Logs;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 import io.appium.java_client.MobileElement;
 
@@ -29,33 +38,44 @@ JSON per creare la sessione con APPIUM
 public class MainTests {
 
     WebDriver driver;
-
+    DesiredCapabilities capabilities;
+    LoggingPreferences logPrefs;
     @Before
     public void setUp() throws MalformedURLException {
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities = new DesiredCapabilities();
+        logPrefs = new LoggingPreferences();
+        logPrefs.enable(LogType.DRIVER, Level.ALL);
 
         //sostiture le capability con quelle relative al vostro simulatore
         capabilities.setCapability("deviceName", "Nexus 5X");
         capabilities.setCapability(CapabilityType.BROWSER_NAME, "Android");
         capabilities.setCapability(CapabilityType.VERSION, "8.1.0");
+        capabilities.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+
         //sostiture le capability con quelle relative al vostro progetto
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("appPackage", "ziviello.com.automation");
         capabilities.setCapability("appActivity", ".MainActivity");
+
         //sostituire il path del vostro server APPIUM nel mio caso e in locale
         driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
     }
 
     @Test
     public void ButtuonsClick() {
 
+
         driver.findElement(By.id("button1")).click();
-        driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
         driver.findElement(By.id("button2")).click();
+        driver.findElement(By.id("button3")).click();
+
+        driver.manage().timeouts().implicitlyWait(50,TimeUnit.SECONDS);
+
     }
+
 
     @After
     public void End() {
